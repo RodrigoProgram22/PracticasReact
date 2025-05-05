67fa85dce71b96c2c26b95a55dc0100454074a5e
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2025-05-05 15:00:24 -0300

    Nueva rama.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-// import { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   let user = "Ezequiel Rodrigo";
-//   const [count, setCount] = useState(0);
-//   const handleClickMas=()=>{
-//     setCount(count+1);
-//   }
-//   const handleClickMenos=()=>{
-//     setCount(count-1);
-//   }
-//   return (
-//     <>
-//     <div className="container">
-//       <h1 >Hola {user}</h1>
-//       <h2>Count {count}</h2>
-//       <button className="button" onClick={handleClickMas}>+</button>
-//       <button className="button" onClick={handleClickMenos}>-</button>
-//     </div>
-//     </>
-//   );
-// }
-// export default App;
-
 import React from "react";
 import "./App.css";
-import { Box, ChakraProvider } from "@chakra-ui/react";
-import ItemListContainer from "./components/ItemListContainer";
-import NavBar from "./components/Navbar";
-import TaskList from "./components/TaskList";
 const App = () => {
   return (
-      <TaskList></TaskList>
+      <>
+        Hola
+      </>
   );
 };
 export default App;
--- src/components/CartWidget.jsx
+++
@@ -1,5 +0,0 @@
-const CartWidget = ()=>{
-    return <div>Carrito:0</div>;
-}
-
-export default CartWidget;
